6adb7e738e27f5195459888d47be261ed8ae3eec
Onur Celik <onrcelik@outlook.com>
2018-02-11 19:12:46 +0300

    app center settings

--- appcenter-post-clone.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Example: Clone a required repository
-git clone https://github.com/onurcelikeng/Evant_Mobile
-
-# Example: Install App Center CLI
-npm install -g appcenter-cli

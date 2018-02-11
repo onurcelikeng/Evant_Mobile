a84a79e39b98272e28c395810adfbc01dfe589e4
Onur Celik <onrcelik@outlook.com>
2018-02-11 19:20:13 +0300

    test commit

--- ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTDatePicker.d
+++ ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTDatePicker.d
@@ -1,10 +1,4 @@
 dependencies: \
-  /Users/onurcelikeng/Documents/Evant/node_modules/react-native/React/Views/RCTDatePicker.m \
-  /Users/onurcelikeng/Documents/Evant/node_modules/react-native/React/Views/RCTDatePicker.h \
-  /Users/onurcelikeng/Documents/Evant/node_modules/react-native/React/Base/RCTUtils.h \
-  /Users/onurcelikeng/Documents/Evant/node_modules/react-native/React/Base/RCTAssert.h \
-  /Users/onurcelikeng/Documents/Evant/node_modules/react-native/React/Base/RCTDefines.h \
-  /Users/onurcelikeng/Documents/Evant/node_modules/react-native/React/Views/UIView+React.h \
-  /Users/onurcelikeng/Documents/Evant/node_modules/react-native/React/Views/RCTComponent.h \
-  /Users/onurcelikeng/Documents/Evant/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/YGEnums.h \
-  /Users/onurcelikeng/Documents/Evant/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/YGMacros.h
+  /Users/onurcelikeng/Documents/Evant_Mobile/node_modules/react-native/React/Views/RCTDatePicker.m \
+  /Users/onurcelikeng/Documents/Evant_Mobile/node_modules/react-native/React/Views/RCTDatePicker.h \
+  /Users/onurcelikeng/Documents/Evant_Mobile/node_modules/react-native/React/Base/RCTUtils.h
--- ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTUIManagerUtils.d
+++ ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTUIManagerUtils.d
@@ -1,3 +1,5 @@
 dependencies: \
   /Users/onurcelikeng/Documents/Evant_Mobile/node_modules/react-native/React/Modules/RCTUIManagerUtils.m \
-  /Users/onurcelikeng/Documents/Evant_Mobile/node_modules/react-native/React/Modules/RCTUIManagerUtils.h
+  /Users/onurcelikeng/Documents/Evant_Mobile/node_modules/react-native/React/Modules/RCTUIManagerUtils.h \
+  /Users/onurcelikeng/Documents/Evant_Mobile/node_modules/react-native/React/Base/RCTAssert.h \
+  /Users/onurcelikeng/Documents/Evant_Mobile/node_modules/react-native/React/Base/RCTDefines.h
